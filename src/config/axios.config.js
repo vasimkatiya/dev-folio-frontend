b935f0caf0cbfef94b969c9054cc9fce68debd93
Vasim Katiya <vasimkatiya97@gmail.com>
2026-06-03 09:43:38 +0530

    cors and axios management

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://dev-folio-back.onrender.com/api",
     withCredentials:true
 });
 
